96391930ff96d9d290ced00b13b4eea949b62c16
Abhitosh Baranwal <abhioshyt@gmail.com>
2026-03-18 18:47:49 +0530

    program to print ‘n’ natural numbers in reverse order

---
+++ forloop_quiz.c
@@ -0,0 +1,12 @@
+/*Quick Quiz: Write a program to print ‘n’ natural numbers in reverse order.*/
+#include<stdio.h>
+int main (){
+    int i = 1,n = 6;
+    for ( i = 1; i < n; i++)
+    {
+        printf("%d\n",i);
+    }
+    
+
+    return 0;
+}
