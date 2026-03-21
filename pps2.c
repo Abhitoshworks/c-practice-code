a42e4ba873f5b1f520095faf46dcf9ae4d464d7e
Abhitosh Baranwal <abhioshyt@gmail.com>
2026-03-21 14:04:49 +0530

    programming problem solving 2

---
+++ pps2.c
@@ -0,0 +1,13 @@
+//Write a program to sum first ten natural numbers using while loop.
+#include<stdio.h>
+int main(){
+    int i = 0;
+    int sum = 0;
+    while(i<10){
+        i++;
+        printf("%d\n",i);
+    }
+
+
+    return 0;
+}
