c36e169f383418c968a1107c39e537b7b847ba26
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-04-06 10:04:54 +0530

    first time interacting with functions

---
+++ function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+    int sum(int,int);
+    int sum(int x,int y){
+        printf("the sum is %d\n",x+y);
+    }
+    int main(){
+    int a = 2;
+    int b = 5;
+    sum(a,b);
+
+    int u = 6;
+    int m = 7;
+    sum(u,m);
+
+
+
+
+    return 0;
+    }
