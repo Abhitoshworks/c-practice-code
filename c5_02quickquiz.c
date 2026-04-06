7a27b13bdd3c7098545f1cce988613d32c67c58e
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-04-06 11:05:18 +0530

    library function to calculate the area of a square

---
+++ c5_02quickquiz.c
@@ -0,0 +1,8 @@
+//Quick Quiz: Use the library function to calculate the area of a square with side a.
+#include<stdio.h>
+#include<math.h>
+int main(){
+int a = 5;
+printf("the area of square is %.2f",pow(a,2));
+return 0;
+}
