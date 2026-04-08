8efcfc66eb6d761ee5e3b01c9560ea792beacefd
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-04-08 18:30:18 +0530

    Write a function to calculate force of attraction on a body of mass ‘m’ exerted by earth. Consider g = 9.8m/s2

---
+++ c5_q3.c
@@ -0,0 +1,13 @@
+/*Write a function to calculate force of attraction on a body of mass ‘m’ exerted by
+earth. Consider g = 9.8m/s2*/
+#include<stdio.h>
+float force(float);
+float force(float m){
+    return m*9.8;
+}
+int main(){
+    int m = 10;
+    printf("%.2f",force(m));
+
+    return 0;
+}
