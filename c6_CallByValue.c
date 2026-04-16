503089bdcd105a9001c3ab5531c7bb6ca059ff43
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-04-16 12:36:30 +0530

    Call by value

---
+++ c6_CallByValue.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+    int sum(int a,int b);
+    int sum(int a,int b){
+        return a + b;
+    }
+    int main(){
+        printf("the sum of 6 and 7 is %d", sum(6,7));
+
+
+    return 0;
+}
