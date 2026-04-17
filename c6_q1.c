cc3c8bc1f9e823b9990bc05c03a04278060c001c
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-04-17 19:53:10 +0530

    program to print the address of a variable

---
+++ c6_q1.c
@@ -0,0 +1,9 @@
+/*Write a program to print the address of a variable. Use this address to get the
+value of the variable.*/
+#include <stdio.h>
+int main(){
+    int i = 6;
+    printf("the address of i is %p",&i);
+
+    return 0;
+}
