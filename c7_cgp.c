dcf3d7145c16ca4672a66acfe76bbf759d578308
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-04-19 22:55:34 +0530

    using for loop with array

---
+++ c7_cgp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int cgpa[3] = {2,4,5};
+    for (int i = 0; i < 3; i++)
+    {
+        printf("cgpa at index %d is %d\n",i,cgpa[i]);
+    }
+    
+
+
+    return 0; 
+}
